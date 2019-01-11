12b7a794d2074b44e423685b732d075332e8e255
ronenbh1 <ronenbh1@gmail.com>
2019-01-11 16:54:31 +0200

    imgjs shows normalized image

---
+++ window.js
@@ -0,0 +1,18 @@
+var remote = require('electron').remote;
+var fs = remote.require('fs');
+const sharp = require('sharp');
+console.time("sharp")
+sharp('test/greymen.png')
+    .normalise()
+    .toBuffer()
+    .then( data => {
+        var _img = data.toString('base64');
+        var _out = '<img src="data:image/png;base64,' + _img + '" />';
+        var _target = document.getElementById('image_container');
+        _target.insertAdjacentHTML('beforeend', _out);
+    })
+    .catch( err => {
+        console.log(err);
+    });												
+ 
+console.timeEnd("sharp")
